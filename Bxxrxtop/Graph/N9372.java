09e900ac4883883471129425a8e81776519d6f3d
백종우 <bsd03291@gmail.com>
2024-04-03 11:45:50 +0900

    [BOJ] 상근이의 여행 / 실버 5
    https://www.acmicpc.net/problem/9372

---
+++ Bxxrxtop/Graph/N9372.java
@@ -0,0 +1,27 @@
+package Graph;
+
+
+
+import java.util.Scanner;
+public class N9372 {
+    // 그래프로 풀이
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 테스트 케이스의 수 T
+        int T = sc.nextInt();
+
+        for(int  i = 0; i < T; i++){
+            // 국가의 수 N
+            int N = sc.nextInt();
+            // 비행기의 종류 M
+            int M = sc.nextInt();
+
+            for(int  j = 0; j < M; j++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+
+            System.out.println(N - 1);
+        }
+    }
+}
